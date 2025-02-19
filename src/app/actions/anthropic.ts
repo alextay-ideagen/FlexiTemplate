46036887ba5f10b07d6edcea797a7f266ec088a6
Alex Tay <alex.tay@ideagenplc.com>
2025-02-19 15:55:50 +0800

    minor change

--- src/app/actions/anthropic.ts
+++ src/app/actions/anthropic.ts
@@ -1,7 +1,6 @@
 'use server';
 
 import AnthropicBedrock from '@anthropic-ai/bedrock-sdk';
-export const maxDuration = 60; // This function can run for a maximum of 60 seconds
 
 // Server Action function to generate AI response
 export async function generateBedrockResponse({
--- src/app/actions/google.ts
+++ src/app/actions/google.ts
@@ -1,7 +1,6 @@
 'use server';
 
 import { GoogleGenerativeAI } from '@google/generative-ai';
-export const maxDuration = 60; // This function can run for a maximum of 60 seconds
 
 export async function getUpdatedHtml({
   prompt,
